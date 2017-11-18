3b68537d1252903f671de1d4fd70c95502b73b28
Tom van Neerijnen <tom@tomvn.com>
2017-11-18 17:04:20 +0000

    Handle yaml bools
    Fixes #5

--- side8/k8s/operator/__init__.py
+++ side8/k8s/operator/__init__.py
@@ -118,6 +118,7 @@ def parse(o, prefix=""):
         return {
             "str": lambda : (prefix, o),
             "int": lambda : parse(str(o), prefix=prefix),
+            "bool": lambda : parse(1 if o else 0, prefix=prefix),
             "NoneType": lambda : parse("", prefix=prefix),
             "list": lambda : flatten([parse(io, "{}{}{}".format(prefix, "_" if prefix else "", ik).upper()) for ik, io in enumerate(o)]),
             "dict": lambda : flatten([parse(io, "{}{}{}".format(prefix, "_" if prefix else "", ik).upper()) for ik, io in o.items()]),
